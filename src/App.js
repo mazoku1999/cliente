4941842578361978458cf9d46d101e4b546268dc
mazoku1999 <rogelio.carata10@gmail.com>
2024-08-26 20:03:04 -0400

    dasddasasdsad
     Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [cargo, setCargo] = useState("");
+  const [antiguedad, setAntiguedad] = useState(0);
+  const [ci, setCi] = useState("");
+
+  const registrar = () => {
+    Axios.post('http://localhost:3001/create', {
+      nombre: nombre,
+      edad: edad,
+      cargo: cargo,
+      antiguedad: antiguedad,
+      ci: ci
+    }).then(() => {
+      alert('Empleado registrado');
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='formulario'>
+        <label>
+          Nombre:
+          <input type='text' onChange={(event) => { setNombre(event.target.value) }}></input>
+        </label>
+        <label>
+          Edad:
+          <input type='number' onChange={(event) => { setEdad(event.target.value) }}></input>
+        </label>
+        <label>
+          Cargo:
+          <input type='text' onChange={(event) => { setCargo(event.target.value) }}></input>
+        </label>
+        <label>
+          Antiguedad:
+          <input type='number' onChange={(event) => { setAntiguedad(event.target.value) }}></input>
+        </label>
+        <label>
+          Ci:
+          <input type='text' onChange={(event) => { setCi(event.target.value) }}></input>
+        </label>
+        <button onClick={registrar}>Registrar</button>
+      </div>
     </div>
   );
 }
